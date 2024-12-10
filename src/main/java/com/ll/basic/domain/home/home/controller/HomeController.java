ba8ed09af7424abfebcf546af90d7d9b6cdbbf74
jhs512 <jangka512@gmail.com>
2024-12-10 12:28:42 +0900

    show about

--- src/main/java/com/ll/basic/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/basic/domain/home/home/controller/HomeController.java
@@ -4,14 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-// @Controller 어노테이션을 붙여주면 스프링부트가 이 클래스를 컨트롤러로 인식하게 된다.
 @Controller
 public class HomeController {
-    // @GetMapping("/") 어노테이션을 붙여주면 이 메소드는 "/" 경로로 들어왔을 때 실행된다.
     @GetMapping("/")
-    // @ResponseBody 어노테이션을 붙여주면 리턴값이 뷰 파일명이 아니라 응답 바디에 들어가게 된다.
     @ResponseBody
     public String showMain() {
         return "안녕하세요.";
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    public String showAbout() {
+        return "저는 홍길동입니다.";
     }
 }

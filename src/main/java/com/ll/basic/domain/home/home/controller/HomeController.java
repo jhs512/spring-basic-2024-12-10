9752ab4ad7116ac4d8b31038446595602017b995
jhs512 <jangka512@gmail.com>
2024-12-10 12:48:46 +0900

    json

--- src/main/java/com/ll/basic/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/basic/domain/home/home/controller/HomeController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class HomeController {
     private int age = 22;
@@ -24,5 +27,67 @@ public class HomeController {
     @ResponseBody
     public int getAgeAndUp() {
         return age++;
+    }
+
+    @GetMapping("/boolean")
+    @ResponseBody
+    public boolean getBoolean() {
+        return true;
+    }
+
+    @GetMapping("/byte")
+    @ResponseBody
+    public byte getByte() {
+        return 127;
+    }
+
+    @GetMapping("/short")
+    @ResponseBody
+    public short getShort() {
+        return 32000;
+    }
+
+    @GetMapping("/long")
+    @ResponseBody
+    public long getLong() {
+        return 100_000_000_000_000L;
+    }
+
+    @GetMapping("/char")
+    @ResponseBody
+    public char getChar() {
+        return '꽑';
+    }
+
+    @GetMapping("/float")
+    @ResponseBody
+    public float getFloat() {
+        return 3.14f;
+    }
+
+    @GetMapping("/double")
+    @ResponseBody
+    public double getDouble() {
+        return 3.141592;
+    }
+
+    @GetMapping("/array")
+    @ResponseBody
+    public String[] getArray() {
+        String[] arr = {"a", "b", "c"};
+
+        return arr;
+    }
+
+    @GetMapping("/list")
+    @ResponseBody
+    public List<String> getList() {
+        return List.of("a", "b", "c");
+    }
+
+    @GetMapping("/map")
+    @ResponseBody
+    public Map<String, String> getMap() {
+        return Map.of("name", "Paul", "hobby", "reading");
     }
 }
